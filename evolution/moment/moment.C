6ee85af2266f8a5f530509535ac1b50f9219cb07
User <user@UsermatoMacBook-Pro-2.local>
2017-12-07 17:55:05 -0500

    a

---
+++ evolution/moment/moment.C
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#include "LHAPDF/LHAPDF.h"
+
+#include "Math/GSLIntegrator.h"
+#include "Math/SpecFuncMathMore.h"
+#include "Math/Interpolator.h"
+
+using namespace std;
+
+int points = 1000;
+
+double xuv(const LHAPDF::PDF * xf);
+double xdv(const LHAPDF::PDF * xf);
+
+int main(const int argc, const char * argv[]){
+
+  LHAPDF::PDF * xpdf;
+
+  xpdf = LHAPDF::mkPDF("NNPDF30_nnlo_as_0118", 0);
+
+  double Eu = xuv(xpdf);
+  double Ed = xdv(xpdf);
+
+  double Vu = 0;
+  double Vd = 0;
+  
+  for (int i = 1; i <= 100; i++){
+    xpdf = LHAPDF::mkPDF("NNPDF30_nnlo_as_0118", i);
+    Vu += pow(xuv(xpdf) - Eu, 2);
+    Vd += pow(xdv(xpdf) - Ed, 2);
+  }
+  Vu = Vu / 100.0;
+  Vd = Vd / 100.0;
+
+  cout << "xu:  " << Eu << "  " << sqrt(Vu) << endl;
+  cout << "xd:  " << Ed << "  " << sqrt(Vd) << endl;
+
+  return 0;
+}
+
+double xuv(const LHAPDF::PDF * xf){
+  double step = 1.0 / points;
+  double x0 = step / 2.0;
+  double sum = 0;
+  for (int i = 0; i < points; i++){
+    sum += xf->xfxQ2(2, x0 + step * i, 10.0) - xf->xfxQ2(-2, x0 + step * i, 10.0);
+  }
+  sum *= step;
+  return sum;
+}
+
+double xdv(const LHAPDF::PDF * xf){
+  double step = 1.0 / points;
+  double x0 = step / 2.0;
+  double sum = 0;
+  for (int i = 0; i < points; i++){
+    sum += xf->xfxQ2(1, x0 + step * i, 10.0) - xf->xfxQ2(-1, x0 + step * i, 10.0);
+  }
+  sum *= step;
+  return sum;
+}
+  
